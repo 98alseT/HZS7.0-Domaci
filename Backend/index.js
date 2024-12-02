f7ae31444ec62cffd822150a837f466e6bd137db
cepStudio <aleksa.cheppy@gmail.com>
2024-12-02 22:28:45 +0100

    Connected to databace 2.0

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const connectDB = require('./config/database');
-const User = require('./models/user_model');
-const subject = require('./models/event_model');
-const learningMaterial = require('./models/learningMaterial_model');
-const SignIn = require('.functions/APIFunctions')
+const SignIn = require('./functions/APIFunctions')
 
 const app = express();
 app.use(express.json());
 
 connectDB();
 
-app.get('/', (req, res) => {
-    res.send('Zdravo!');
+app.post('/api/newuser', async (req, res) => {
+    await SignIn(req, res);
 });
-
-app.post('/api/newuser', SignIn);
 
 app.listen(3000, () => {
     console.log('Server je pokrenut na portu 3000');
