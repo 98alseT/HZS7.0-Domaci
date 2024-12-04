5767e489b7fdcc47a9fb86c1f9ed730ab2dcdb7e
98alseT <tesla89karate@gmail.com>
2024-12-05 00:33:42 +0100

    LogIn & SignUp Page

--- Frontend/src/Pages/LogIn.jsx
+++ Frontend/src/Pages/LogIn.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import '../PagesCSS/LogInSignIn.css';
 import { useNavigate } from 'react-router-dom'
 
 const SignIn = () => {
   const navigate = useNavigate();
 
   return (
-    <div>LogIn</div>
+    <div className="main-container">
+      <h1 className="title">Log In</h1>
+        <form>
+          <input type="text" placeholder="username" required />
+          <input type="password" placeholder="password" required />
+          <button type="submit">LOGIN</button>
+        </form>
+      <p className="register">
+        Not registered? <a href="#" onClick={() => navigate('/sign-in')}>Create an account</a>
+      </p>
+    </div>
   )
 }
 
--- Frontend/src/Pages/SignIn.jsx
+++ Frontend/src/Pages/SignIn.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import '../PagesCSS/LogInSignIn.css';
 import { useNavigate } from 'react-router-dom'
 
 const SignIn = () => {
   const navigate = useNavigate();
 
   return (
-    <div>SignIn</div>
+    <div className="main-container">
+      <h1 className="title">Sign Up</h1>
+        <form>
+          <input type="text" placeholder="username" required />
+          <input type="password" placeholder="password" required />
+          <button type="submit">Sign Up</button>
+        </form>
+      <p className="register">
+        Have an account? <a href="#" onClick={() => navigate('/log-in')}>Log In</a>
+      </p>
+    </div>
   )
 }
 
